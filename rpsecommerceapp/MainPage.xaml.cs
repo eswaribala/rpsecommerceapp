a174f75f03c8a248a7f258d9c7371f2d4b9c3865
eswaribala <parameswaribala@gmail.com>
2021-10-20 00:04:36 +0530

    updated

--- rpsecommerceapp/MainPage.xaml.cs
+++ rpsecommerceapp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using rpsecommerceapp.views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -22,21 +23,30 @@ namespace rpsecommerceapp
             ColorList.Add(Color.Magenta);
             ColorList.Add(Color.Violet);
             ColorList.Add(Color.Pink);
-
-           // RowDefinition row = new RowDefinition();
-            //row.Height = new GridLength(1.0, GridUnitType.Star);
+            List<String> imageNames = new List<string>();
+            imageNames.Add("mobile.jpg");
+            imageNames.Add("tv.jpg");
+            imageNames.Add("smartwatch.jpg");
+            imageNames.Add("washingmachine.jpg");
+            imageNames.Add("mobile.jpg");
+            imageNames.Add("tv.jpg");
+            imageNames.Add("smartwatch.jpg");
+            imageNames.Add("washingmachine.jpg");
+            /*
+            RowDefinition row = new RowDefinition();
+            row.Height = new GridLength(1.0, GridUnitType.Star);
            
-          //  NewArrivalGrid.RowDefinitions.Add(new RowDefinition
-         //  { Height=new GridLength(1.0, GridUnitType.Star) });
-           // NewArrivalGrid.RowDefinitions.Add(new RowDefinition
-           // { Height=new GridLength(1.0, GridUnitType.Star) });
-          //  NewArrivalGrid.RowDefinitions.Add(new RowDefinition
-          //  { Height = new GridLength(1.0, GridUnitType.Star) });
-          // NewArrivalGrid.ColumnDefinitions.Add(new ColumnDefinition
-          // { Width= new GridLength(1.0, GridUnitType.Star) });
-           //NewArrivalGrid.ColumnDefinitions.Add(new ColumnDefinition{
-           //Width= new GridLength(1.0, GridUnitType.Star) });
-
+            NewArrivalGrid.RowDefinitions.Add(new RowDefinition
+           { Height=new GridLength(1.0, GridUnitType.Star) });
+            NewArrivalGrid.RowDefinitions.Add(new RowDefinition
+            { Height=new GridLength(1.0, GridUnitType.Star) });
+            NewArrivalGrid.RowDefinitions.Add(new RowDefinition
+            { Height = new GridLength(1.0, GridUnitType.Star) });
+           NewArrivalGrid.ColumnDefinitions.Add(new ColumnDefinition
+           { Width= new GridLength(1.0, GridUnitType.Star) });
+          NewArrivalGrid.ColumnDefinitions.Add(new ColumnDefinition{
+          Width= new GridLength(1.0, GridUnitType.Star) });
+           */
             for (int rowIndex = 0; rowIndex < 7; rowIndex++)
             {
                 for (int columnIndex = 0; columnIndex < 2; columnIndex++)
@@ -44,23 +54,58 @@ namespace rpsecommerceapp
 
                     var box = new BoxView
                     {
-                        BackgroundColor = ColorList[rowIndex]
+                        BackgroundColor = Color.White,
+                        WidthRequest=50,
+                        HeightRequest=50
 
                     };
-                    var label = new Label
+                    var image = new Image
                     {
-                        Text="XAMRAIN"
+                        Source = imageNames[rowIndex],
+                        HeightRequest=50,
+                        WidthRequest=50
                     };
                     NewArrivalGrid.Children.Add(box, columnIndex, rowIndex);
-                    NewArrivalGrid.Children.Add(label, columnIndex, rowIndex);
+                    NewArrivalGrid.Children.Add(image, columnIndex, rowIndex);
                 }
             }
-            
+
+            var button = new Button
+            {
+                Text = "Visit Store",
+                BackgroundColor = Color.Navy,
+                TextColor = Color.White,
+                //WidthRequest=50,
+                //HeightRequest=50
+                
+                
+            };
+            button.Clicked += async delegate
+              {
+                  await Navigation.PushModalAsync(new NavigationPage(new StoreTab()));
+              };
+
+            var stackLayout = new StackLayout();
+           // stackLayout.WidthRequest = 50;
+            //stackLayout.HeightRequest = 50;
+            stackLayout.Children.Add(button);
+            topStack.Children.Add(stackLayout);
+
         }
 
-        //private void Button_Clicked(object sender, EventArgs e)
-        //{
-        //    Navigation.PushModalAsync(new NavigationPage(new StoreTab()));
-        //}
+        protected async override void OnAppearing()
+        {
+           base.OnAppearing();
+            await Task.Delay(10000);
+            await Navigation.PushModalAsync(new NavigationPage(new StoreTab()));
+        }
+        /*
+        private void Store_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushModalAsync(new NavigationPage(new StoreTab()));
+        }
+        */
     }
+
+   
 }
---
+++ rpsecommerceapp/views/StoreTab.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace rpsecommerceapp.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StoreTab : TabbedPage
+    {
+        public StoreTab()
+        {
+            InitializeComponent();
+            Label ProductLabel1 = new Label
+            {
+                Text = "Robotic Vaccum Cleaner",
+                FontSize = 40,
+                FontAttributes = FontAttributes.Bold,
+                //HorizontalTextAlignment = TextAlignment.Center
+            };
+            Label ProductLabel2 = new Label
+            {
+                Text = "X-Ray",
+                FontSize = 40,
+                FontAttributes = FontAttributes.Bold,
+                //HorizontalTextAlignment = TextAlignment.Center
+            };
+            var ProductRealativeLayout = new RelativeLayout();
+            ProductRealativeLayout.Children.Add(ProductLabel1, Constraint.Constant(50),
+                Constraint.RelativeToParent((parent) =>
+                {
+                    return parent.Height / 4;
+                }));
+            ProductRealativeLayout.Children.Add(ProductLabel2,
+               Constraint.Constant(0),
+               Constraint.RelativeToView(ProductLabel1, (parent, sibling) => {
+                   return sibling.Height / 2;
+               }));
+            ProductGallery.Content = ProductRealativeLayout;
+            Label CartLabel = new Label
+            {
+                Text = "My Cart",
+                FontSize = 40,
+                FontAttributes = FontAttributes.Bold,
+                //HorizontalTextAlignment = TextAlignment.Center
+            };
+            var CartRealativeLayout = new RelativeLayout();
+            CartRealativeLayout.Children.Add(CartLabel, Constraint.Constant(50),
+                Constraint.RelativeToParent((parent) =>
+                {
+                    return parent.Height / 4;
+                }));
+
+            Cart.Content = CartRealativeLayout;
+
+            Label OrderLabel = new Label
+            {
+                Text = "My Orders",
+                FontSize = 40,
+                FontAttributes = FontAttributes.Bold,
+                //HorizontalTextAlignment = TextAlignment.Center
+            };
+            var OrderRealativeLayout = new RelativeLayout();
+            OrderRealativeLayout.Children.Add(OrderLabel, Constraint.Constant(75),
+                Constraint.RelativeToParent((parent) =>
+                {
+                    return parent.Height / 6;
+                }));
+
+            Orders.Content = OrderRealativeLayout;
+        }
+    }
+}
