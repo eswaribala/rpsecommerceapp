8291df22a918cd8e382104e5811cd7fa3931b571
eswaribala <parameswaribala@gmail.com>
2021-10-22 00:20:27 +0530

    updated

---
+++ rpsecommerceapp/views/CaruoselDemoPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using rpsecommerceapp.viewmodels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace rpsecommerceapp.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CaruoselDemoPage : ContentPage
+    {
+        public CaruoselDemoPage()
+        {
+            InitializeComponent();
+            var vm = new ProductViewModel();
+            this.BindingContext = vm;
+        }
+    }
+}
